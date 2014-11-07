ff6ae0d1e3fccf0f4208a075d2fb0d593c3e84db
Natacha Marlio-Marette <natacha.marlio-marette@etu.univ-tours.fr>
2014-11-07 11:42:44 +0100

    ajout de drawFiliaireCache

--- srcstudent/Renderer.cpp
+++ srcstudent/Renderer.cpp
@@ -1,6 +1,8 @@
 #include <Renderer.h>
 #include <Panel3D.h>
 #include <Engine.h>
+#include <stdio.h>
+using namespace std;
 
 void Renderer::DrawFilaire()
 {
@@ -8,25 +10,43 @@ void Renderer::DrawFilaire()
     Coord3D p1, p2, p3;
     Color c1, c2, c3;
     Face f;
+    Coord2D m1, m2, m3;
+
+    // Transformation du Drawable
+    // Calcul de ses informations dans le repère de la camera
+
+    Camera cam;
+    // Calcul la position et l'orientation de la caméra pour que l'objet drawable exprimé dans le repère global soit entièrement visible.
+    cam.SetFromDrawable(*drawable);
+    Rotation rot;
+    effectiveDrawable->Compute(*drawable, cam, rot);
 
 	// L'objet a dessiné est dans l'attribut drawable
 	// on parcourt le tableau contenant la liste des faces/triangles a dessiné
 	for(int i=0; i< drawable->faces.size; i++){
-       f = drawable->faces.data[i];
+        f = drawable->faces.data[i];
         //coord  et couleur point 1
-       p1 = drawable->points.data[f.index1];
-       c1 = drawable->pointColors.data[f.index1];
+        p1 = drawable->points.data[f.index1];
+        c1 = drawable->pointColors.data[f.index1];
+        m1.x = p1.x;
+        m1.y = p1.y;
 
-       // coord  et couleur point 2
-       p2 = drawable->points.data[f.index2];
-       c2 = drawable->pointColors.data[f.index2];
-       // coord  et couleur point 3
-       p3 = drawable->points.data[f.index3];
-       c3 = drawable->pointColors.data[f.index3];
+        // coord  et couleur point 2
+        p2 = drawable->points.data[f.index2];
+        c2 = drawable->pointColors.data[f.index2];
+        m2.x = p2.x;
+        m2.y = p2.y;
 
-//       buffer->DrawLine(p1, p2, c1, c2);
-//       buffer->DrawLine(p1, p3, c1, c3);
-//       buffer->DrawLine(p2, p3, c2, c3);
+        // coord  et couleur point 3
+        p3 = drawable->points.data[f.index3];
+        c3 = drawable->pointColors.data[f.index3];
+        m3.x = p3.x;
+        m3.y = p3.y;
+
+
+       buffer->DrawLine(m1, m2, c1, c2);
+       buffer->DrawLine(m1, m3, c1, c3);
+       buffer->DrawLine(m2, m3, c2, c3);
 	}
 
 
@@ -34,6 +54,54 @@ void Renderer::DrawFilaire()
 void Renderer::DrawFilaireCache()
 {
 	// compléter ici
+    Coord3D p1, p2, p3;
+    Color c1, c2, c3;
+    Face f;
+    Coord2D m1, m2, m3;
+    int index;
+
+    // Transformation du Drawable
+    // Calcul de ses informations dans le repère de la camera
+
+    Camera cam;
+    // Calcul la position et l'orientation de la caméra pour que l'objet drawable exprimé dans le repère global soit entièrement visible.
+    cam.SetFromDrawable(*drawable);
+    Rotation rot;
+    effectiveDrawable->Compute(*drawable, cam, rot);
+
+	// L'objet a dessiné est dans l'attribut drawable
+	// on parcourt le tableau contenant la liste des faces/triangles a dessiné
+	cout<< "debut boucle affichage"<<endl;
+	cout<< "taille : " << effectiveDrawable->sortedVisibleFaces.size << endl;
+	for(int i=(effectiveDrawable->sortedVisibleFaces.size -1); i>=0; i--){
+        index = effectiveDrawable->sortedVisibleFaces.data[i].index;
+
+        f = drawable->faces.data[index];
+        //coord  et couleur point 1
+        p1 = drawable->points.data[f.index1];
+        c1 = drawable->pointColors.data[f.index1];
+        m1.x = p1.x;
+        m1.y = p1.y;
+
+        // coord  et couleur point 2
+        p2 = drawable->points.data[f.index2];
+        c2 = drawable->pointColors.data[f.index2];
+        m2.x = p2.x;
+        m2.y = p2.y;
+
+        // coord  et couleur point 3
+        p3 = drawable->points.data[f.index3];
+        c3 = drawable->pointColors.data[f.index3];
+        m3.x = p3.x;
+        m3.y = p3.y;
+
+
+       buffer->DrawLine(m1, m2, c1, c2);
+       buffer->DrawLine(m1, m3, c1, c3);
+       buffer->DrawLine(m2, m3, c2, c3);
+	}
+
+
 }
 void Renderer::DrawFacePleine()
 {
