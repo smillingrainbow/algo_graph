43df90012d709fb979b703056861b91e53aeb339
Maxoum <maxime.Facomprez@hotmail.com>
2014-12-14 13:16:23 +0100

    Update App.cpp

--- srcstudent/App.cpp
+++ srcstudent/App.cpp
@@ -13,17 +13,17 @@ bool App::OnInit()
 {
 	//Exception::activateDebugHelper();
 
-	// on initialise les gestionnaire de format d'images
+	// On initialise les gestionnaire de format d'images
 	wxInitAllImageHandlers();
-	// on initialise le générateur de nombre aléatoire
+	// On initialise le générateur de nombre aléatoire
 	srand( (unsigned) time(NULL));
-	// on force la locale à "C" (anglais ansi)
+	// On force la locale à "C" (anglais ansi)
 	setlocale(LC_NUMERIC, "C");
 
-	// on crée la fenêtre principale
+	// On crée la fenêtre principale
 	MainFrame * mf = new MainFrame(true);
-	// on affiche la fenêtre principale
+	// On affiche la fenêtre principale
 	mf->Show();
-	// on retourne true pour indiquer que tout c'est bien déroulé
+	// On retourne true pour indiquer que tout c'est bien déroulé
 	return true;
 }
